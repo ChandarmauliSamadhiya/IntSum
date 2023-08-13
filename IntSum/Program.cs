c47399a5d3a63c0569ef00b0f53f991d9071d83e
acer <acer@ASUR>
2023-08-13 21:11:25 +0530

    Add project files.

---
+++ IntSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine("Sum of elements: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
